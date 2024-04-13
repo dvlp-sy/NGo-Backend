f8bd32dc76db1e05765ef48131df854766d1e922
dvlp-sy <sy8389b@gmail.com>
2024-04-13 09:53:22 +0900

    [add] Initialize and Add Dependency

---
+++ ngo/src/main/java/com/ngo/NgoApplication.java
@@ -0,0 +1,13 @@
+package com.ngo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgoApplication.class, args);
+	}
+
+}
