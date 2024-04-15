56824faae34e356cec5a7a6308d2bba62105b7db
dvlp-sy <sy8389b@gmail.com>
2024-04-15 22:58:35 +0900

    [chore] Add ApiResponse Format

---
+++ ngo/src/main/java/com/ngo/common/message/ErrorMessage.java
@@ -0,0 +1,46 @@
+package com.ngo.common.message;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import static org.springframework.http.HttpStatus.*;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public enum ErrorMessage
+{
+    /**
+     * Login
+     */
+
+    /**
+     * User
+     */
+
+    /**
+     * News
+     */
+
+    /**
+     * Scrap
+     */
+
+    /**
+     * Dictionary
+     */
+
+    /**
+     * Rank
+     */
+
+    ;
+
+    private final HttpStatus httpStatus;
+    private final String message;
+
+    public int getHttpStatusCode()
+    {
+        return httpStatus.value();
+    }
+}
