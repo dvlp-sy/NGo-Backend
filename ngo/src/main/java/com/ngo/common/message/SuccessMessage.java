4b66a97d243ddf08ce6e20256cd65fe4cd1cf230
dvlp-sy <sy8389b@gmail.com>
2024-04-21 09:53:37 +0900

    [add] Add Success and Error Message

--- ngo/src/main/java/com/ngo/common/message/SuccessMessage.java
+++ ngo/src/main/java/com/ngo/common/message/SuccessMessage.java
@@ -30,7 +30,8 @@ public enum SuccessMessage
      * Scrap
      */
 
-    POST_SCRAP_SUCCESS(CREATED, "스크랩 업로드에 성공했습니다")
+    POST_SCRAP_SUCCESS(CREATED, "스크랩 업로드에 성공했습니다"),
+    DELETE_SCRAP_SUCCESS(NO_CONTENT, "스크랩 삭제에 성공했습니다")
 
     /**
      * Dictionary
