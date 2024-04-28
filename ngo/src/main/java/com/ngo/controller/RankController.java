8ce4ff20d310f874c36367c95527255b05327167
dvlp-sy <sy8389b@gmail.com>
2024-04-28 10:15:03 +0900

    [feat] Implement getDailyRank

--- ngo/src/main/java/com/ngo/controller/RankController.java
+++ ngo/src/main/java/com/ngo/controller/RankController.java
@@ -1,8 +1,10 @@
 package com.ngo.controller;
 
 import com.ngo.common.ApiResponse;
+import com.ngo.dto.RankDto;
 import com.ngo.dto.ScoreDto;
 import com.ngo.service.RankService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +15,12 @@ public class RankController
     private final RankService rankService;
 
     public RankController(RankService rankService) { this.rankService = rankService; }
+
+    @GetMapping("/user/{userId}/daily-rank")
+    public ApiResponse<RankDto> getDailyRank(@PathVariable("userId") Long userId)
+    {
+        return rankService.getDailyRank(userId);
+    }
 
     @PatchMapping("/user/{userId}/rank")
     public ApiResponse<ScoreDto> patchScore(@PathVariable("userId") Long userId)
--- ngo/src/main/java/com/ngo/service/RankService.java
+++ ngo/src/main/java/com/ngo/service/RankService.java
@@ -4,12 +4,17 @@ import com.ngo.common.ApiResponse;
 import com.ngo.common.exception.NotFoundException;
 import com.ngo.common.message.ErrorMessage;
 import com.ngo.common.message.SuccessMessage;
+import com.ngo.dto.RankDto;
 import com.ngo.dto.ScoreDto;
+import com.ngo.dto.UserRankDto;
 import com.ngo.model.User;
 import com.ngo.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class RankService {
@@ -17,6 +22,36 @@ public class RankService {
 
     public RankService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public ApiResponse<RankDto> getDailyRank(Long userId)
+    {
+        RankDto rankDto = new RankDto();
+
+        userRepository.findById(userId)
+                .orElseThrow(() -> new NotFoundException(ErrorMessage.USER_NOT_FOUND));
+
+        List<User> userList = userRepository.findAll().stream()
+                .sorted(Comparator.comparing(User::getDayScore).reversed())
+                .toList();
+
+        long userRank = 0L;
+        List<UserRankDto> userRankDtoList = new ArrayList<>();
+        for (User user : userList)
+        {
+            userRank++;
+            userRankDtoList.add(new UserRankDto(user.getUserId(), userRank, user.getUserName()));
+            if (userId.equals(user.getUserId()))
+            {
+                rankDto.setUserId(userId);
+                rankDto.setUserName(user.getUserName());
+                rankDto.setUserRank(userRank);
+            }
+        }
+
+        rankDto.setUserRankDtoList(userRankDtoList);
+        return ApiResponse.success(SuccessMessage.GET_RANK_SUCCESS, rankDto);
+
     }
 
     public ApiResponse<ScoreDto> patchScore(Long userId) {
