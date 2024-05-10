9d0b3ddb01bf9fea9c51996736f8f1c6732d8680
dvlp-sy <sy8389b@gmail.com>
2024-05-10 12:59:37 +0900

    [add] Add Success Message

--- ngo/src/main/java/com/ngo/common/message/SuccessMessage.java
+++ ngo/src/main/java/com/ngo/common/message/SuccessMessage.java
@@ -31,6 +31,7 @@ public enum SuccessMessage
      * News
      */
     POST_TODAY_NEWS_SUCCESS(CREATED, "오늘의 뉴스 생성에 성공했습니다"),
+    GET_TODAY_NEWS_SUCCESS(OK, "오늘의 뉴스 불러오기에 성공했습니다"),
 
     /**
      * Scrap
