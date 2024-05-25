5c095a6aa15bc125bec270d2a748b11db452c55e
dvlp-sy <sy8389b@gmail.com>
2024-05-25 20:30:07 +0900

    [refactor] Refactor MediaRepository

--- ngo/src/main/java/com/ngo/repository/MediaRepository.java
+++ ngo/src/main/java/com/ngo/repository/MediaRepository.java
@@ -1,10 +1,12 @@
 package com.ngo.repository;
 
+import com.ngo.model.Media;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Map;
 
 @Repository
-public interface MediaRepository
+public interface MediaRepository extends JpaRepository<Media, Long>
 {
-    Map<String, String> getAllMedia();
+
 }
