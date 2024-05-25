224bc0b67fc69b05ca5b3e747d19090656d1f958
dvlp-sy <sy8389b@gmail.com>
2024-05-25 21:16:52 +0900

    [docs] Create Docs for Repository

--- ngo/src/main/java/com/ngo/repository/MediaRepository.java
+++ ngo/src/main/java/com/ngo/repository/MediaRepository.java
@@ -5,6 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Map;
 
+/**
+ * ==== MediaRepository Interface ====
+ * @package : com.ngo.repository
+ * @name : MediaRepository.java
+ * @date : 2024. 05. 10.
+ * @author : siyunsmacbook
+*/
+
 @Repository
 public interface MediaRepository extends JpaRepository<Media, Long>
 {
