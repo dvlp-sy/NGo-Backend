50c1192e688176ec697bdaaad98005a6c70dc621
dvlp-sy <sy8389b@gmail.com>
2024-06-09 11:07:05 +0900

    [fix] Fix Codes

--- ngo/src/main/java/com/ngo/controller/LoginController.java
+++ ngo/src/main/java/com/ngo/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.ngo.controller;
 
 import com.ngo.common.ApiResponse;
 import com.ngo.dto.requestDto.LoginFormDto;
+import com.ngo.dto.responseDto.UserDto;
 import com.ngo.service.LoginService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -27,7 +28,7 @@ public class LoginController
     }
 
     @PostMapping("/users/login")
-    public ApiResponse<Void> loginUser(@RequestBody LoginFormDto loginFormDto, HttpServletRequest request, HttpServletResponse response)
+    public ApiResponse<UserDto> loginUser(@RequestBody LoginFormDto loginFormDto, HttpServletRequest request, HttpServletResponse response)
     {
         return loginService.loginUser(loginFormDto, request, response);
     }
